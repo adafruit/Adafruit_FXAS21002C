f87984fbd39619b79b2c25608905e0f2f5f75c7a
Doxygen CI <ci-arduino@invalid>
2021-04-06 01:14:42 +0000

    Deploy docs to GitHub Pages from commit 9d49d02565
    Commit: 9d49d02565fa3e9bf195edacc7ddfd8eb8805447
    GitHub Actions run: 719995905

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['adafruit_5ffxas21002c',['Adafruit_FXAS21002C',['../class_adafruit___f_x_a_s21002_c.html',1,'Adafruit_FXAS21002C'],['../class_adafruit___f_x_a_s21002_c.html#ab70f2d06df57966b07d9b3fbdaeabaf5',1,'Adafruit_FXAS21002C::Adafruit_FXAS21002C()']]],
+  ['adafruit_5ffxas21002c',['Adafruit_FXAS21002C',['../class_adafruit___f_x_a_s21002_c.html',1,'Adafruit_FXAS21002C'],['../class_adafruit___f_x_a_s21002_c.html#a643aecd2215c6adf06f0a2b0cf3535a4',1,'Adafruit_FXAS21002C::Adafruit_FXAS21002C()']]],
   ['adafruit_5ffxas21002c_2ecpp',['Adafruit_FXAS21002C.cpp',['../_adafruit___f_x_a_s21002_c_8cpp.html',1,'']]],
   ['adafruit_5ffxas21002c_2eh',['Adafruit_FXAS21002C.h',['../_adafruit___f_x_a_s21002_c_8h.html',1,'']]],
   ['adafruit_20fxas21002c_20gyroscope_20sensor_20driver',['Adafruit FXAS21002C gyroscope sensor driver',['../index.html',1,'']]]
--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_5ffxas21002c',['Adafruit_FXAS21002C',['../class_adafruit___f_x_a_s21002_c.html#ab70f2d06df57966b07d9b3fbdaeabaf5',1,'Adafruit_FXAS21002C']]]
+  ['adafruit_5ffxas21002c',['Adafruit_FXAS21002C',['../class_adafruit___f_x_a_s21002_c.html#a643aecd2215c6adf06f0a2b0cf3535a4',1,'Adafruit_FXAS21002C']]]
 ];
